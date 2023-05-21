82c260b5dbe2274b499b1eae49f1a78c4cef87b3
MaryShapiro <Mashynya9@yandex.ru>
2023-05-22 00:05:26 +0300

    Упорядочиваем элементы каждой строки массива

---
+++ Task_54/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+   Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+   Например, задан массив:
+   1 4 7 2
+   5 9 2 3
+   8 4 2 4
+   В итоге получается вот такой массив:
+   7 4 2 1
+   9 5 3 2
+   8 4 4 2
+*/
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] mass = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            mass[i, j] = new Random().Next(min, max + 1);
+
+        }
+    }
+    return mass;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortingArrayRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите количетсво столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 10, 99);
+Console.WriteLine("Исходный массив: ");
+PrintArray(array);
+
+SortingArrayRows(array);
+Console.WriteLine("Массив, отсортированный по убыванию: ");
+PrintArray(array);
